ba1d8f7eba7892ac124d862d48c9cc16e47e9a5f
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-08-26 12:35:30 +0500

    remove all auth functionality except login

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\ImageController::class, 'index']);
 
-Auth::routes();
+Route::get('login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route::post('logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
 
 Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
 Route::post('/image', [App\Http\Controllers\ImageController::class, 'store'])->name('image');
